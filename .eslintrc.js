9a19481c5d92ab653d3409a7f5c3d22f5699ae82
leeheeae <chromeee@daum.net>
2021-12-11 18:09:51 +0900

    type설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,23 +6,24 @@ module.exports = {
         jest: true,
     },
     extends: [
+        'eslint:recommended',
         'plugin:@typescript-eslint/eslint-recommended',
         'plugin:@typescript-eslint/recommended',
     ],
     plugins: ['prettier', '@typescript-eslint'],
     rules: {
-        'prettier/prettier': [
-            'error',
-            {
-                singleQuote: true,
-                semi: true,
-                useTabs: false,
-                tabWidth: 2,
-                printWidth: 80,
-                bracketSpacing: true,
-                arrowParens: 'avoid',
-            },
-        ],
+        // 'prettier/prettier': [
+        //     'error',
+        //     {
+        //         singleQuote: true,
+        //         semi: true,
+        //         useTabs: false,
+        //         tabWidth: 2,
+        //         printWidth: 80,
+        //         bracketSpacing: true,
+        //         arrowParens: 'avoid',
+        //     },
+        // ],
         '@typescript-eslint/no-explicit-any': 'off',
         'prefer-const': 'off',
     },
